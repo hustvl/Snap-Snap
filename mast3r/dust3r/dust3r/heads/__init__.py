f053d623b2545bc464310e1998d30ff41646786a
陆佳 <lujia@lujiadeMacBook-Air.local>
2025-09-01 22:24:26 +0800

    import mast3r as local code with my modifications

---
+++ mast3r/dust3r/dust3r/heads/__init__.py
@@ -0,0 +1,23 @@
+# Copyright (C) 2024-present Naver Corporation. All rights reserved.
+# Licensed under CC BY-NC-SA 4.0 (non-commercial use only).
+#
+# --------------------------------------------------------
+# head factory
+# --------------------------------------------------------
+from .linear_head import LinearPts3d
+from .dpt_head import create_dpt_head, create_color_head, create_dpt_head_concat
+
+
+def head_factory(head_type, output_mode, net, has_conf=False):
+    """" build a prediction head for the decoder 
+    """
+    if head_type == 'linear' and output_mode == 'pts3d':
+        return LinearPts3d(net, has_conf)
+    elif head_type == 'dpt' and output_mode == 'pts3d':
+        return create_dpt_head(net, has_conf=has_conf)
+    elif head_type == 'dpt' and output_mode == 'pts3d_concat':
+        return create_dpt_head_concat(net, has_conf=has_conf)
+    elif head_type == 'dpt' and output_mode == 'color':
+        return create_color_head(net)
+    else:
+        raise NotImplementedError(f"unexpected {head_type=} and {output_mode=}")
